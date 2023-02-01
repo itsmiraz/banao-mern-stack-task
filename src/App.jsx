8b75c34b5a4372149f9c485a2ebcfd030a0b8242
itsmiraz <mirajulislamofficial044@gmail.com>
2023-02-01 21:46:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Banner from './Components/Header/Banner/Banner'
+import Header from './Components/Header/Header'
+
+function App() {
+
+  return (
+    <section>
+      <Header></Header>
+      <Banner></Banner>
+    </section>
+  )
+}
+
+export default App
---
+++ src/Components/Header/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Banner.css'
+const Banner = () => {
+    return (
+        <section className='banner'>
+            <div className="title">
+                <h1 className='fw-semibold mt-5'>Computer Engineering</h1>
+                <p>
+                    142,765 Computer Engineers follow this
+                </p>
+            </div>
+        </section>
+    );
+};
+
+export default Banner;
